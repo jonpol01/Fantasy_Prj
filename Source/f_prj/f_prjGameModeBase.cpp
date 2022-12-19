ce9d0ce1bab452656d9d6eeb79e89882596d7de9
John Paul Soliva <soliva@seaos.co.jp>
2022-12-19 23:39:38 +0900

    ADD landscape, Character

---
+++ Source/f_prj/f_prjGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "f_prjGameModeBase.h"
+
---
+++ Source/f_prj/f_prjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "f_prjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class F_PRJ_API Af_prjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
